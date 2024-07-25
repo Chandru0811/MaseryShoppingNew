18ad68048393d575393ab4e125eeb51710b8bce0
Sridaran04dec <sri04122001@gmail.com>
2024-07-25 18:56:17 +0530

    updated

--- app/Http/Controllers/Api/Admin/OrderController.php
+++ app/Http/Controllers/Api/Admin/OrderController.php
@@ -15,13 +15,31 @@ class OrderController extends Controller
     {
         $orders = Order::all();
 
-        return $this->success('Order Retrived Successfully',$orders);
+        return $this->success('Order Retrived Successfully', $orders);
     }
 
     public function show($id)
     {
-        $order = Order::with('inventories.image:path,imageable_id,imageable_type')->where('id',$id)->first();
-        
-        return $this->success('Order Retrived Successfully',$order);
+        $order = Order::with('inventories.image:path,imageable_id,imageable_type')->where('id', $id)->first();
+
+        return $this->success('Order Retrived Successfully', $order);
+    }
+
+    public function store($id)
+    {
+        $order = Order::with('inventories.image:path,imageable_id,imageable_type')->where('id', $id)->first();
+
+        return $this->success('Order Retrived Successfully', $order);
+    }
+
+    public function orderByCustomerId($customerId)
+    {
+        $orders = Order::with('inventories.image:path,imageable_id,imageable_type')->where('customer_id', $customerId)->get();
+
+        if ($orders->isEmpty()) {
+            return $this->error('No orders found for the specified customer ID', 404);
+        }
+
+        return $this->success('Orders Retrieved Successfully', $orders);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,55 +19,58 @@ use App\Http\Controllers\Api\Admin\AccountController;
 Route::post('login', [AuthController::class, 'login']);
 Route::post('register', [AuthController::class, 'register']);
 //HomePage
-Route::get('homescreen', [HomeController::class,'index']);
-Route::get('categories', [HomeController::class,'getcategory']);
-Route::get('brands', [HomeController::class,'getbrands']);
-Route::post('contact', [UserController::class,'contact']);
- Route::get('get/product/{slug}', [HomeController::class,'product']);
+Route::get('homescreen', [HomeController::class, 'index']);
+Route::get('categories', [HomeController::class, 'getcategory']);
+Route::get('brands', [HomeController::class, 'getbrands']);
+Route::post('contact', [UserController::class, 'contact']);
+Route::get('get/product/{slug}', [HomeController::class, 'product']);
 // CART
-Route::post('addToCart/{slug}', [CartController::class,'addToCart']);
-Route::delete('cart/removeItem', [CartController::class,'remove']);
-Route::get('carts', [CartController::class,'index']);
-Route::put('cart/{cart}/update', [CartController::class,'update']);
-Route::get('totalitems',[CartController::class,'totalitemsincart']);
-Route::get('search',[HomeController::class,'search']);
-Route::get('cart/{cart}/checkout', [CheckoutController::class,'getcheckoutdetails']);
+Route::post('addToCart/{slug}', [CartController::class, 'addToCart']);
+Route::delete('cart/removeItem', [CartController::class, 'remove']);
+Route::get('carts', [CartController::class, 'index']);
+Route::put('cart/{cart}/update', [CartController::class, 'update']);
+Route::get('totalitems', [CartController::class, 'totalitemsincart']);
+Route::get('search', [HomeController::class, 'search']);
+Route::get('cart/{cart}/checkout', [CheckoutController::class, 'getcheckoutdetails']);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    
-    Route::post('logout',[AuthController::class,'logout']);
+    Route::get('Customers', [AccountController::class, 'index']);
+    Route::get('orders/customer/{customerId}', [OrderController::class, 'orderByCustomerId']);
+
+
+    Route::post('logout', [AuthController::class, 'logout']);
 
     Route::middleware('role:1')->group(function () {
         Route::get('/admin', function () {
             return response()->json(['message' => 'Welcome Admin']);
         });
-        
-        
+
+
         Route::apiResource('banner', BannerController::class);
         Route::apiResource('brand', BrandController::class);
         Route::apiResource('category', CategoryController::class);
         Route::apiResource('about', AboutController::class);
         Route::apiResource('product', ProductController::class);
-        Route::get('inventory/add/{inventory}', [InventoryController::class,'add']);
-        Route::post('inventory/store', [InventoryController::class,'store']);
-        Route::get('inventory/index', [InventoryController::class,'index']);
-        Route::post('inventory/{inventory}/update', [InventoryController::class,'update']);
-        Route::post('inventory/{inventory}/delete', [InventoryController::class,'destroy']);
-        Route::get('inventory/{inventory}/get', [InventoryController::class,'show']);
-        Route::get('orders', [OrderController::class,'index']);
-		Route::get('order/{order}', [OrderController::class,'show']);
-		Route::get('Customers', [AccountController::class,'index']);
-		Route::get('contactedUsers', [AccountController::class,'contactedUsers']);
+        Route::get('inventory/add/{inventory}', [InventoryController::class, 'add']);
+        Route::post('inventory/store', [InventoryController::class, 'store']);
+        Route::get('inventory/index', [InventoryController::class, 'index']);
+        Route::post('inventory/{inventory}/update', [InventoryController::class, 'update']);
+        Route::post('inventory/{inventory}/delete', [InventoryController::class, 'destroy']);
+        Route::get('inventory/{inventory}/get', [InventoryController::class, 'show']);
+        Route::get('orders', [OrderController::class, 'index']);
+        Route::get('order/{order}', [OrderController::class, 'show']);
+        Route::get('Customers', [AccountController::class, 'index']);
+        Route::get('contactedUsers', [AccountController::class, 'contactedUsers']);
     });
 
     Route::middleware('role:2')->group(function () {
         Route::get('/user/dashboard', function () {
             return response()->json(['message' => 'Welcome User']);
         });
-        
-        Route::post('cart/{cart}/checkout', [CheckoutController::class,'checkout']);
+
+        Route::post('cart/{cart}/checkout', [CheckoutController::class, 'checkout']);
     });
 });
