b048706a805ea2ca32d4a0c67fecdacfe227399c
Chandru0811 <chandru08112000@gmail.com>
2024-09-10 18:01:52 +0530

    committed

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,15 +14,15 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         // Create the top-level category
-        $category = Category::create(['name' => 'Motherboard']);
+        $category = Category::create(['name' => 'Motherboard', 'slug' => 'motherboard']);
 
         // Create the subcategory AMD under Motherboard
-        $sub_category = Category::create(['name' => 'AMD', 'parent_id' => $category->id]);
+        $sub_category = Category::create(['name' => 'AMD', 'slug' => 'amd', 'parent_id' => $category->id]);
 
         // Create the sub-subcategory AMD 500 Series under AMD
-        $sub_category1 = Category::create(['name' => 'AMD 500 Series', 'parent_id' => $sub_category->id]);
+        $sub_category1 = Category::create(['name' => 'AMD 500 Series', 'slug' => 'amd_500_series', 'parent_id' => $sub_category->id]);
 
         // Create the sub-sub-subcategory ASUS under AMD 500 Series
-        $sub_category2 = Category::create(['name' => 'ASUS', 'parent_id' => $sub_category1->id]);
+        $sub_category2 = Category::create(['name' => 'ASUS', 'slug' => 'asus', 'parent_id' => $sub_category1->id]);
     }
 }
